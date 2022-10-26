a527d1750e64e98a8e67c8dcc16da2fb335946c9
AmirMMoghaddam <amirmohammad.moghaddam2@gmail.com>
2022-10-26 14:21:35 +0330

    Added a bit bash

--- bash/dddd/renamer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for file in *.*; do
-    mv "$file" "${file/_half_half./_Hi.}"
-done
--- bash/renamer.sh
+++ bash/renamer.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-for file in *.*; do
-    mv "$file" "${file/_half_half./_Hi.}"
-done
+
+DESDIR=$1
+COM="mv"
+if [ $2 == "-c" ]; then
+    COM="cp"
+fi
+DATEFILE=$(date +%Y-%m-%d_)
+ListFile=$(ls $DESDIR)
+if [ -d $DESDIR ]; then
+    for file in $ListFile; do
+        echo
+        $COM "$DESDIR/$file" "$DESDIR/$DATEFILE$file"
+    done
+else
+    echo "the directory does not exist!"
+fi
