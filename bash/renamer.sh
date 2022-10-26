7be7ce5dbae15484f585e93fc3f2645aaf4383d4
AmirMMoghaddam <amirmohammad.moghaddam2@gmail.com>
2022-10-26 18:21:22 +0330

    Completed Bash !

--- bash/renamer.sh
+++ bash/renamer.sh
@@ -2,15 +2,28 @@
 
 DESDIR=$1
 COM="mv"
-if [ $2 == "-c" ]; then
+TexT="changing name to: "
+case $2 in
+"-c")
     COM="cp"
-fi
+    TexT="making copy: "
+    ;;
+"--copy")
+    COM="cp"
+    TexT="making copy: "
+    ;;
+esac
 DATEFILE=$(date +%Y-%m-%d_)
 ListFile=$(ls $DESDIR)
 if [ -d $DESDIR ]; then
+    echo "directory submitted successfully!"
     for file in $ListFile; do
-        echo
+
         $COM "$DESDIR/$file" "$DESDIR/$DATEFILE$file"
+        filename=$(basename -- "$file")
+        extension="${filename##*.}"
+        echo "name: $file    type: .$extension       ->   $TexT $DATEFILE$file"
+
     done
 else
     echo "the directory does not exist!"
